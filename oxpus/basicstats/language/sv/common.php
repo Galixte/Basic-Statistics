c2fc094a48b100233fecfe358b0591a00515b39f
Maskinisten <info@gremminger.de>
2015-10-09 16:58:56 +0200

    Swedish translation

---
+++ oxpus/basicstats/language/sv/common.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+*
+* @package phpBB Extension - Basic Stats
+* @copyright (c) 2015 OXPUS - www.oxpus.net
+* @Swedish translation by Holger (https://www.maskinisten.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/*
+* [ swedish ] language file
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+   	'BASIC_STATS'	=> 'Forumstatistik',
+   	'DAILY'			=> 'Daglig statistik',
+   	'MONTHLY'		=> 'Månadsstatistik',
+   	'YEARLY'		=> 'Årlig statistik',
+
+	'ALL'					=> 'Alla',
+	'AS_ON'					=> 'Stånd %s',	
+	'AVG_POSTS_DAY'			=> 'Genomsnittligt antal inlägg per dag',	
+	'AVG_POSTS_MONTH'		=> 'Genomsnittligt antal inlägg per månad',	
+	'AVG_POSTS_YEAR'		=> 'Genomsnittligt antal inlägg per år',	
+	'AVG_TOPICS_DAY'		=> 'Genomsnittligt antal ämnen per dag',
+	'AVG_TOPICS_MONTH'		=> 'Genomsnittligt antal ämnen per månad',
+	'AVG_TOPICS_YEAR'		=> 'Genomsnittligt antal ämnen per år',
+	'AVG_USER_REGS_DAY'		=> 'Genomsnittligt antal registreringar per dag',
+	'AVG_USER_REGS_MONTH'	=> 'Genomsnittligt antal registreringar per månad',
+	'AVG_USER_REGS_YEAR'	=> 'Genomsnittligt antal registreringar per år',
+	'COUNT'					=> 'Antal',
+	'D_VIEW'				=> 'Daglig statistik',
+	'M_VIEW'				=> 'Månadsstatistik',
+	'NONE'					=> 'Inga',
+	'OVERALL'				=> 'Totalt',
+	'PERCENT'				=> 'Procent',
+	'PERIODIC_DAY'			=> 'Dag',
+	'PERIODIC_MONTH'		=> 'Månad',
+	'PERIODIC_YEAR'			=> 'År',
+	'SHOW_STATS_FOR_MONTH'	=> 'Visa statistik för den valda månaden',
+	'SHOW_STATS_FOR_YEAR'	=> 'Visa statistik för det valda året',
+	'STATS_MONTH_EXPLAIN'	=> 'Följande statistik gäller för månad <strong>%s</strong>',
+	'STATS_YEAR_EXPLAIN'	=> 'Följande statistik gäller för år <strong>%s</strong>',
+	'TOPICS'				=> 'Ämnen',
+	'TOTAL_POSTS'			=> 'Totalt antal inlägg',
+	'TOTAL_TOPICS'			=> 'Totalt antal ämnen',
+	'TOTAL_USER_REGS'		=> 'Totalt antal registreringar',
+	'USER_REGS'				=> 'Registreringar',
+	'Y_VIEW'				=> 'Årlig statistik',
+
+));
